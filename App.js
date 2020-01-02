2a007c8716425282f12dedb7ca460f9c07f774fe
zeenayouhan <zinayouhan33@gmail.com>
2020-01-03 04:18:45 +0530

    fouthcommit

--- App.js
+++ App.js
@@ -17,15 +17,52 @@
 //console.log(__filename);
 //console.log(__dirname);
 
+
+//path module
 //const path = require('path');
 
 //var pathobj= path.parse(__filename);
 //console.log(pathobj);
 
-var os = require('os');
 
-var totalmemory= os.totalmem();
-var freeMem= os.freemem();
+//os module
+//var os = require('os');
 
-console.log(`TotalMemory ${totalmemory}`);
-console.log(`FreeMemory ${freeMem}`);
+//var totalmemory= os.totalmem();
+//var freeMem= os.freemem();
+
+//console.log(`TotalMemory ${totalmemory}`);
+//console.log(`FreeMemory ${freeMem}`);
+
+//filesystem module
+//const fs= require('fs');
+
+//const file=fs.readdirSync('./');
+
+//console.log(file);
+
+//fs.readdir('./j',function(err,files){
+   // if(err) console.log("Error",err);
+ //   else console.log('Result',files);
+//})
+
+//event module
+const EventEmitter = require('events');
+
+const emmiter= new EventEmitter();
+
+//emmiter.on('MessageLogged',function(){
+//    console.log('Worked');
+//});
+
+//emmiter.emit('MessageLogged');
+
+//Event Arguments
+emmiter.on('MessageLogged',(arg)=>{
+    console.log("listner",arg);
+});
+
+emmiter.emit('MessageLogged',{
+    id:1,
+    url:'zeena'
+});
